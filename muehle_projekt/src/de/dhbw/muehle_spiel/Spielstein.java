3c999c4c8468b79de2aaec04ea97732747511e7e
maklose <marvin.klose92@gmail.com>
2013-09-18 13:41:04 +0200

    Methoden erstellt und erste Methoden gefüllt

--- muehle_projekt/src/de/dhbw/muehle_spiel/Spielstein.java
+++ muehle_projekt/src/de/dhbw/muehle_spiel/Spielstein.java
@@ -7,10 +7,62 @@ public class Spielstein implements ISpielstein, IBewegung
 	//Variablen
 	private ESpielsteinFarbe Spielsteinfarbe;
 	private boolean gesperrt;
-	private Position akutellePosition;
+	private Position aktuellePosition;
 	private boolean ausgewählt;
 	
+	public Spielstein(ESpielsteinFarbe lSpielsteinfarbe, Position laktuellePosition)
+	{
+		Spielsteinfarbe = lSpielsteinfarbe;
+		aktuellePosition = laktuellePosition; 
+		gesperrt = false;
+		ausgewählt = false;
+	}	
 	
+	public void bewegen(Position nachPosition)
+	{
+		aktuellePosition = nachPosition; 
+	}
+	
+	public ESpielsteinFarbe getSpielsteinfarbe() 
+	{
+		return Spielsteinfarbe;
+	}
+
+	public void setSpielsteinfarbe(ESpielsteinFarbe spielsteinfarbe) 
+	{
+		Spielsteinfarbe = spielsteinfarbe;
+	}
+
+	public boolean isGesperrt() 
+	{
+		return gesperrt;
+	}
+
+	public void setGesperrt(boolean gesperrt) 
+	{
+		this.gesperrt = gesperrt;
+	}
+
+	public Position getAkutellePosition() 
+	{
+		return aktuellePosition;
+	}
+
+	public void setAkutellePosition(Position akutellePosition) 
+	{
+		this.aktuellePosition = akutellePosition;
+	}
+
+	public boolean isAusgewählt() 
+	{
+		return ausgewählt;
+	}
+
+	public void setAusgewählt(boolean ausgewählt) 
+	{
+		this.ausgewählt = ausgewählt;
+	}
+
 	@Override
 	public Position altePosition() 
 	{
@@ -39,14 +91,8 @@ public class Spielstein implements ISpielstein, IBewegung
 		return null;
 	}
 
-	public Spielstein()
-	{
-		
-	}
 	
-	public void bewegen(Position nachPosition)
-	{
-		
-	}
+	
+	
 	
 }
