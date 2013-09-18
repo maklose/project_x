c0ce26c3059056093e9e00c803a0946dfb2aabe0
Philipp <philippgerds@gmail.com>
2013-09-18 14:01:25 +0200

    Implementierung Spieler

--- muehle_projekt/src/de/dhbw/muehle_spiel/Spieler.java
+++ muehle_projekt/src/de/dhbw/muehle_spiel/Spieler.java
@@ -1,6 +1,30 @@
 package de.dhbw.muehle_spiel;
 import de.dhbw.muehle_api.*;
+import de.dhbw.muehle_api.strategy.IBewegung;
+import de.dhbw.muehle_api.strategy.ISpielzug;
 
-public class Spieler {
+public class Spieler implements ISpielzug  {
 
+private int AnzahlZüge;  // Anzahl der Züge die ein Spieler gemacht hat
+private int AnzahlSteine;  //Anzahl der Steine, die ein Spieler auf dem Spielfeld hat
+ESpielsteinFarbe Spielerfarbe; //Farbe der Spielsteine eines Spielers
+EPhase Phase; //Spielphase, in der sich der Spieler befindet
+
+//Konstruktor, Erzeugung eines neuen Spielers unter Angabe der Spielerfarbe
+public Spieler (ESpielsteinFarbe lSpielerfarbe)
+{
+	Spielerfarbe = lSpielerfarbe;
+	AnzahlZüge = 0;
+}
+
+	public ISpielstein getNeuenSpielstein() {
+		AnzahlZüge = AnzahlZüge + 1;
+	}
+
+	@Override
+	public IBewegung bewegeSpielStein() {
+		AnzahlZüge = AnzahlZüge + 1;
+	}
+
+	
 }
