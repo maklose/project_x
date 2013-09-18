4d6e9a1fbb7d3c8cd492ed11c84d4410e8b210ac
maklose <marvin.klose92@gmail.com>
2013-09-18 13:26:13 +0200

    Variablen und Methoden im Spielstein erstellt

--- muehle_projekt/src/de/dhbw/muehle_spiel/Spielstein.java
+++ muehle_projekt/src/de/dhbw/muehle_spiel/Spielstein.java
@@ -1,5 +1,52 @@
 package de.dhbw.muehle_spiel;
+import de.dhbw.muehle_api.*;
+import de.dhbw.muehle_api.strategy.IBewegung;
 
-public class Spielstein {
+public class Spielstein implements ISpielstein, IBewegung
+{
+	//Variablen
+	private ESpielsteinFarbe Spielsteinfarbe;
+	private boolean gesperrt;
+	private Position akutellePosition;
+	private boolean ausgewählt;
+	
+	
+	@Override
+	public Position altePosition() 
+	{
+		// TODO Auto-generated method stub
+		return null;
+	}
 
+	@Override
+	public Position neuePosition() 
+	{
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public ESpielsteinFarbe getFarbe() 
+	{
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Position getPosition() 
+	{
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public Spielstein()
+	{
+		
+	}
+	
+	public void bewegen(Position nachPosition)
+	{
+		
+	}
+	
 }
