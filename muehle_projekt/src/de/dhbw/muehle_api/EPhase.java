2231cef85d34e630cf60248fe512ca286546ac89
Philipp <philippgerds@gmail.com>
2013-09-18 14:02:45 +0200

    Enumeration Spielphase erstellt

---
+++ muehle_projekt/src/de/dhbw/muehle_api/EPhase.java
@@ -0,0 +1,18 @@
+package de.dhbw.muehle_api;
+
+
+// Enumeration der drei Phasen des Spielsase 
+public enum EPhase{
+	
+	//Erste Phase: Setzen der Steine
+Setzen,
+
+	//Zweite Phase: Alle Spielsteine sind gelegt worden
+	//Steine können jetzt geschoben werden
+Schieben,
+
+	//Dritte Phase: Anzahl der Spielsteine <= 3
+	//Steine können jetzt springen
+Legen
+	
+}
