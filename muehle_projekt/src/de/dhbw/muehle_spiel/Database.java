6542469c2774a49a1b74c09911f270a1fe43a9e4
zyrus <getraenkeheini@web.de>
2013-09-19 13:13:47 +0200

    delete mydb

--- muehle_projekt/src/de/dhbw/muehle_spiel/Database.java
+++ muehle_projekt/src/de/dhbw/muehle_spiel/Database.java
@@ -7,7 +7,7 @@ import java.sql.SQLException;
 public class Database {
 	Class.forName("org.apache.derby.jdbc.EmbeddedDriver");
 	//Verbindung zur Datenbank
-	Connection connection;
+	Connection c;
 	connection=DriverManager.getConnection("jdbc:derby:MyDB");
 	
 }
