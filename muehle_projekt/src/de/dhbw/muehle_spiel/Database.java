4ffb8e2641ea9b8403a1dad3fdf33ce09f2f7d90
zyrus <getraenkeheini@web.de>
2013-09-25 20:46:54 +0200

    Löschung von Derby

--- muehle_projekt/src/de/dhbw/muehle_spiel/Database.java
+++ muehle_projekt/src/de/dhbw/muehle_spiel/Database.java
@@ -1,18 +1,34 @@
 package de.dhbw.muehle_spiel;
 
-import java.beans.Statement;
+
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.sql.Statement;
+import java.io.File;
+
 
 public class Database {
-	private final String database= "MyDB";
+	String database= "MyDB";
 	
-	//Verbindung zur Datenbank
-	Class.forName("org.apache.derby.jdbc.EmbeddedDriver").newInstance();
-	Connection c = DriverManager.getConnection("jdbc:derby:"+database+"create=true");
-	Statement s = (Statement) c.createStatement();
-	s.executeUpdate("CREATE TABLE spielzug " + "(spielstein varchar(20), e1 int, x1 int, y1 int, e2 int, x2 int, y2 int)");
-	s.executeUpdate("INSERT INTO spielzug (spielstein, e1, x1, y1, e2, x2, y2)" + "VALUES ('weiﬂ1', 1,2,3,4,5,6)");
+	Connection c;
+	
+	public void start() throws InstantiationException, IllegalAccessException, ClassNotFoundException, SQLException {
+		Class.forName("org.apache.derby.jdbc.EmbeddedDriver");
+		if (new File(database).exists()) {
+			c = DriverManager.getConnection("jdbc:derby:" + database);
+		} else {
+			c = DriverManager.getConnection("jdbc:derby:" + database + ";create=true");
+			filldb(c);
+		}
+	}
+	
+	public static void filldb(Connection c) throws SQLException{
+		Statement s = (Statement) c.createStatement();
+		s.executeUpdate("CREATE TABLE spielzug " + "(spielstein varchar(20), e1 int, x1 int, y1 int, e2 int, x2 int, y2 int)");
+		s.executeUpdate("INSERT INTO spielzug"+ "VALUES ('weiﬂ1', 1,2,3,4,5,6)");
+	}
+
+
 }
 
