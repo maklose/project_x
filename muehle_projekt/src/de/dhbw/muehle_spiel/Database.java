d1a238c40f69858d564e34293fb715fd9904764a
zyrus <getraenkeheini@web.de>
2013-09-27 14:02:43 +0200

    db

--- muehle_projekt/src/de/dhbw/muehle_spiel/Database.java
+++ muehle_projekt/src/de/dhbw/muehle_spiel/Database.java
@@ -1,34 +1,57 @@
 package de.dhbw.muehle_spiel;
 
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
-import java.sql.Statement;
-import java.io.File;
+import java.sql.*;
+import java.io.*;
 
 
 public class Database {
-	String database= "MyDB";
-	
-	Connection c;
-	
-	public void start() throws InstantiationException, IllegalAccessException, ClassNotFoundException, SQLException {
-		Class.forName("org.apache.derby.jdbc.EmbeddedDriver");
-		if (new File(database).exists()) {
-			c = DriverManager.getConnection("jdbc:derby:" + database);
-		} else {
-			c = DriverManager.getConnection("jdbc:derby:" + database + ";create=true");
-			filldb(c);
+	public static void main(String[]args){
+		String driver="com.mysql.jdbc.Driver";
+		String url= "jdbc:mysql://localhost/Namen";
+		String user = "root";
+		String password="database";
+		
+		try{
+			Class.forName(driver);
+			
 		}
+		catch(Exception e){
+			System.err.println(e.getMessage());
+			System.exit(0);
+		}
+	
+		Connection conn = null;
+		Statement hans= null;
+		try{
+			conn=DriverManager.getConnection("jdbc:mysql://localhost/Namen?user=root&password=database");
+			hans=conn.createStatement();
+			String query="SELECT * FROM Biersorten";
+			ResultSet result= hans.executeQuery(query);
+			if(result!=null){
+				ResultSetMetaData rsmd=result.getMetaData();
+				for(int i=1; i<=rsmd.getColumnCount();i++){
+					System.out.print(rsmd.getColumnName(i));
+					System.out.print((char)9);
+					
+				}
+				System.out.println();
+				
+				result.beforeFirst();
+				while(result.next()){
+					for(int column=1; column<=rsmd.getColumnCount();column++){
+						System.out.print(result.getObject(column));
+						System.out.print((char)9);
+						System.out.println();
+					}
+				}	result.close();
+			}
+		
+		}
+		catch(SQLException)
+	
 	}
 	
-	public static void filldb(Connection c) throws SQLException{
-		Statement s = (Statement) c.createStatement();
-		s.executeUpdate("CREATE TABLE spielzug " + "(spielstein varchar(20), e1 int, x1 int, y1 int, e2 int, x2 int, y2 int)");
-		s.executeUpdate("INSERT INTO spielzug"+ "VALUES ('weiß1', 1,2,3,4,5,6)");
-	}
-
-
+	
 }
 
