31fd54072a03cc4cea76192ddf53361438906ef9
zyrus <getraenkeheini@web.de>
2013-10-09 21:55:29 +0200

    db change

--- muehle_projekt/src/de/dhbw/muehle_spiel/Database.java
+++ muehle_projekt/src/de/dhbw/muehle_spiel/Database.java
@@ -16,9 +16,13 @@ public class Database {
 		    
 	}
 //Löschen der Datenbank	
-public void deletedb(Statement statement){
-	String delete=("DROP DATABASE database");
+public void deletetb(){
+	String url="jdbc:sqlite:database.db";
+	String delete=("DROP TABLE position");
 	try {
+		Connection c = null;		    
+	    c = DriverManager.getConnection(url);
+	    Statement statement=c.createStatement();
 		statement.executeQuery(delete);
 	} catch (SQLException e) {		
 		e.printStackTrace();
@@ -26,17 +30,75 @@ public void deletedb(Statement statement){
 	
 	
 }
+
+public void testdelete(){
+	String driver="org.sqlite.JDBC";
+	String url="jdbc:sqlite:database.db";
+	String delete=("DROP TABLE position");
+	try {
+		Class.forName(driver);
+		Connection c = null;		    
+	    c = DriverManager.getConnection(url);
+	    Statement statement=c.createStatement();
+		statement.executeQuery(delete);
+	} catch (SQLException e) {		
+		e.printStackTrace();
+	} catch (ClassNotFoundException e) {
+		
+		e.printStackTrace();
+	}
+}
 //Übergabe der Position an DB
-public void valuetrans(Statement statement){
-	String update=("INSERT INTO position VALUES('Weiß',5,5,5,6,6,6)");
+public void valuetrans(){
+	String url="jdbc:sqlite:database.db";
+	String update=("INSERT INTO position (Spielstein,E1,X1,Y1,E2,X2,Y2) VALUES('Weiß',5,5,5,6,6,6)");
     try {
+    	Connection c = null;		    
+	    c = DriverManager.getConnection(url);
+	    Statement statement=c.createStatement();
 		statement.executeUpdate(update);
 	} catch (SQLException e) {
 		e.printStackTrace();
 	}
 }
+
+//Ausgabe der DB
+public void showdb(){
+	String url="jdbc:sqlite:database.db";
+    try {
+    	Connection c = null;		    
+	    c = DriverManager.getConnection(url);
+	    Statement statement=c.createStatement();
+	    String ausgabe=("SELECT * FROM position");
+	    ResultSet result=statement.executeQuery(ausgabe);
+	    while (result.next()) {
+	    	  System.out.println();
+	    	  int zugID = result.getInt("ID");
+	    	  System.out.print("ID: "+zugID);
+	    	  String Spielstein=result.getString("Spielstein");
+	    	  System.out.print(" Spielstein: "+Spielstein);
+	    	  int E1=result.getInt("E1");
+	    	  int X1=result.getInt("X1");
+	    	  int Y1=result.getInt("Y1");
+	    	  int E2=result.getInt("E2");
+	    	  int X2=result.getInt("X2");
+	    	  int Y2=result.getInt("Y2");
+	    	  System.out.print(" E1: "+E1);
+	    	  System.out.print(" X1: "+X1);
+	    	  System.out.print(" Y1: "+Y1);
+	    	  System.out.print(" E2: "+E2);
+	    	  System.out.print(" X2: "+X2);
+	    	  System.out.print(" Y2: "+Y2);
+	    	  
+	    	  
+	    	  
+	    	}
+	} catch (SQLException e) {
+		e.printStackTrace();
+	}
+}
 //Erzeugen der DB
-public void createdb(){
+public void connectdb(){
 	String driver="org.sqlite.JDBC";
 	String url="jdbc:sqlite:database.db";
 		
@@ -44,18 +106,16 @@ public void createdb(){
 		    try {
 		    Class.forName(driver);
 		      
-		 		    
 		    Connection c = null;		    
 		    c = DriverManager.getConnection(url);
 		    Statement statement= null;
 		    statement = c.createStatement();
-		    
-		    String create="CREATE TABLE position (ID INT AUTO_INCREMENT,"
-		    			+ "Spielstein varChar(30),"
-		    			+ "E1 INT, X1 INT, Y1 INT, "
-		    			+ "E2 INT, X2 INT, Y2 INT) ";
-		    statement.executeQuery(create);
-		    		  		    
+//		    String create="CREATE TABLE position(ID INTEGER PRIMARY KEY AUTOINCREMENT,"
+//		    			+ "Spielstein varChar(30),"
+//		    			+ "E1 INT, X1 INT, Y1 INT, "
+//		    			+ "E2 INT, X2 INT, Y2 INT) ";
+//		    statement.executeQuery(create);
+		    		    		  		    
 		    } 
 		    catch ( Exception e ) {
 		    	System.err.println(e.getMessage());
--- muehle_projekt/src/de/dhbw/muehle_spiel/testderdb.java
+++ muehle_projekt/src/de/dhbw/muehle_spiel/testderdb.java
@@ -1,6 +1,13 @@
 package de.dhbw.muehle_spiel;
 
 public class testderdb {
-Database db= new Database();
+	public static void main(String[]args){
+		Database db= new Database();
+		//db.testdelete();
+		db.connectdb();
+		db.valuetrans();
+		db.showdb();
+		
+	}
 
 }
