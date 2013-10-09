6993f2c9365030cd8a9e5b91d7ba46212013acb0
maklose <marvin.klose92@gmail.com>
2013-10-09 13:51:27 +0200

    ActionListener hinzugefügt

--- muehle_projekt/src/de/dhbw/gui/Spielfeld2.java
+++ muehle_projekt/src/de/dhbw/gui/Spielfeld2.java
@@ -15,6 +15,7 @@ import java.awt.Color;
 import javax.swing.JLabel;
 
 
+
 import net.miginfocom.swing.MigLayout;
 
 import java.awt.GridLayout;
@@ -22,6 +23,7 @@ import java.awt.GridLayout;
 import javax.swing.JButton;
 
 import de.dhbw.muehle_api.*;
+import de.dhbw.muehle_util.plugin.MyServiceLoader;
 
 public class Spielfeld2 extends JFrame {
 
@@ -30,13 +32,20 @@ public class Spielfeld2 extends JFrame {
 	/**
 	 * Launch the application.
 	 */
-	public static void main(String[] args) {
-		EventQueue.invokeLater(new Runnable() {
-			public void run() {
-				try {
-					Spielfeld2 frame = new Spielfeld2();
+	
+	public static void main(String[] args) 
+	{
+		final mActionListener lSteuerung = new mActionListener();
+		EventQueue.invokeLater(new Runnable() 
+		{
+			public void run() 
+			{
+				try 
+				{
+					Spielfeld2 frame = new Spielfeld2(lSteuerung);
 					frame.setVisible(true);
-				} catch (Exception e) {
+				} catch (Exception e) 
+				{
 					e.printStackTrace();
 				}
 			}
@@ -47,7 +56,8 @@ public class Spielfeld2 extends JFrame {
 	/**
 	 * Create the frame.
 	 */
-	public Spielfeld2() {
+	public Spielfeld2(mActionListener Steuerung) 
+	{
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 450, 450);
 		contentPane = new JPanel();
@@ -73,6 +83,7 @@ public class Spielfeld2 extends JFrame {
 	
 		
 		JButton btnNewButton_1 = new TransparentButtonFeld("", new Position(EPositionIndex.Eins, EPositionIndex.Eins, EPositionIndex.Drei));
+		btnNewButton_1.addActionListener(Steuerung);
 		panel.add(btnNewButton_1);
 		
 		JLabel lblNewLabel = new JLabel("");
@@ -82,6 +93,7 @@ public class Spielfeld2 extends JFrame {
 		panel.add(label);
 		
 		JButton btnNewButton_4 = new TransparentButtonFeld("", new Position(EPositionIndex.Eins, EPositionIndex.Zwei, EPositionIndex.Drei));
+		btnNewButton_4.addActionListener(Steuerung);
 		panel.add(btnNewButton_4);
 		
 		JLabel lblNewLabel_1 = new JLabel("");
@@ -91,24 +103,28 @@ public class Spielfeld2 extends JFrame {
 		panel.add(lblNewLabel_2);
 		
 		JButton btnNewButton_6 = new TransparentButtonFeld("", new Position(EPositionIndex.Eins, EPositionIndex.Drei, EPositionIndex.Drei));
+		btnNewButton_6.addActionListener(Steuerung);
 		panel.add(btnNewButton_6);
 		
 		JLabel label_1 = new JLabel("");
 		panel.add(label_1);
 		
 		JButton btnNewButton_9 = new TransparentButtonFeld("", new Position(EPositionIndex.Zwei, EPositionIndex.Eins, EPositionIndex.Drei));
+		btnNewButton_9.addActionListener(Steuerung);
 		panel.add(btnNewButton_9);
 		
 		JLabel label_2 = new JLabel("");
 		panel.add(label_2);
 		
 		JButton btnNewButton_10 = new TransparentButtonFeld("", new Position(EPositionIndex.Zwei, EPositionIndex.Zwei, EPositionIndex.Drei));
+		btnNewButton_10.addActionListener(Steuerung);
 		panel.add(btnNewButton_10);
 		
 		JLabel lblNewLabel_3 = new JLabel("");
 		panel.add(lblNewLabel_3);
 		
 		JButton btnNewButton_13 = new TransparentButtonFeld("", new Position(EPositionIndex.Zwei, EPositionIndex.Drei, EPositionIndex.Drei));
+		btnNewButton_13.addActionListener(Steuerung);
 		panel.add(btnNewButton_13);
 		
 		JLabel label_3 = new JLabel("");
@@ -121,12 +137,15 @@ public class Spielfeld2 extends JFrame {
 		panel.add(label_5);
 		
 		JButton btnNewButton_15 = new TransparentButtonFeld("", new Position(EPositionIndex.Drei, EPositionIndex.Eins, EPositionIndex.Drei));
+		btnNewButton_15.addActionListener(Steuerung);
 		panel.add(btnNewButton_15);
 		
 		JButton btnNewButton_18 = new TransparentButtonFeld("", new Position(EPositionIndex.Drei, EPositionIndex.Zwei, EPositionIndex.Drei));
+		btnNewButton_18.addActionListener(Steuerung);
 		panel.add(btnNewButton_18);
 		
 		JButton btnNewButton_19 = new TransparentButtonFeld("", new Position(EPositionIndex.Drei, EPositionIndex.Drei, EPositionIndex.Drei));
+		btnNewButton_19.addActionListener(Steuerung);
 		panel.add(btnNewButton_19);
 		
 		JLabel label_6 = new JLabel("");
@@ -136,24 +155,30 @@ public class Spielfeld2 extends JFrame {
 		panel.add(label_7);
 		
 		JButton btnNewButton_22 = new TransparentButtonFeld("", new Position(EPositionIndex.Eins, EPositionIndex.Eins, EPositionIndex.Zwei));
+		btnNewButton_22.addActionListener(Steuerung);
 		panel.add(btnNewButton_22);
 		
 		JButton btnNewButton_23 = new TransparentButtonFeld("", new Position(EPositionIndex.Zwei, EPositionIndex.Eins, EPositionIndex.Zwei));
+		btnNewButton_23.addActionListener(Steuerung);
 		panel.add(btnNewButton_23);
 		
 		JButton btnNewButton_24 = new TransparentButtonFeld("", new Position(EPositionIndex.Drei, EPositionIndex.Eins, EPositionIndex.Zwei));
+		btnNewButton_24.addActionListener(Steuerung);
 		panel.add(btnNewButton_24);
 		
 		JLabel label_8 = new JLabel("");
 		panel.add(label_8);
 		
 		JButton btnNewButton_26 = new TransparentButtonFeld("", new Position(EPositionIndex.Drei, EPositionIndex.Drei, EPositionIndex.Zwei));
+		btnNewButton_26.addActionListener(Steuerung);
 		panel.add(btnNewButton_26);
 		
 		JButton btnNewButton_27 = new TransparentButtonFeld("", new Position(EPositionIndex.Zwei, EPositionIndex.Drei, EPositionIndex.Zwei));
+		btnNewButton_27.addActionListener(Steuerung);
 		panel.add(btnNewButton_27);
 		
 		JButton btnNewButton_28 = new TransparentButtonFeld("", new Position(EPositionIndex.Eins, EPositionIndex.Drei, EPositionIndex.Zwei));
+		btnNewButton_28.addActionListener(Steuerung);
 		panel.add(btnNewButton_28);
 		
 		JLabel label_9 = new JLabel("");
@@ -163,12 +188,15 @@ public class Spielfeld2 extends JFrame {
 		panel.add(label_10);
 		
 		JButton btnNewButton_31 = new TransparentButtonFeld("", new Position(EPositionIndex.Drei, EPositionIndex.Eins, EPositionIndex.Eins));
+		btnNewButton_31.addActionListener(Steuerung);
 		panel.add(btnNewButton_31);
 		
 		JButton btnNewButton_32 = new TransparentButtonFeld("", new Position(EPositionIndex.Drei, EPositionIndex.Zwei, EPositionIndex.Eins));
+		btnNewButton_32.addActionListener(Steuerung);
 		panel.add(btnNewButton_32);
 		
 		JButton btnNewButton_33 = new TransparentButtonFeld("", new Position(EPositionIndex.Drei, EPositionIndex.Drei, EPositionIndex.Eins));
+		btnNewButton_33.addActionListener(Steuerung);
 		panel.add(btnNewButton_33);
 		
 		JLabel label_11 = new JLabel("");
@@ -181,24 +209,28 @@ public class Spielfeld2 extends JFrame {
 		panel.add(label_13);
 		
 		JButton btnNewButton_37 = new TransparentButtonFeld("", new Position(EPositionIndex.Zwei, EPositionIndex.Eins, EPositionIndex.Eins));
+		btnNewButton_37.addActionListener(Steuerung);
 		panel.add(btnNewButton_37);
 		
 		JLabel label_14 = new JLabel("");
 		panel.add(label_14);
 		
 		JButton btnNewButton_39 = new TransparentButtonFeld("", new Position(EPositionIndex.Zwei, EPositionIndex.Zwei, EPositionIndex.Eins));
+		btnNewButton_39.addActionListener(Steuerung);
 		panel.add(btnNewButton_39);
 		
 		JLabel label_15 = new JLabel("");
 		panel.add(label_15);
 		
 		JButton btnNewButton_41 = new TransparentButtonFeld("", new Position(EPositionIndex.Zwei, EPositionIndex.Drei, EPositionIndex.Eins));
+		btnNewButton_41.addActionListener(Steuerung);
 		panel.add(btnNewButton_41);
 		
 		JLabel label_16 = new JLabel("");
 		panel.add(label_16);
 		
 		JButton btnNewButton_43 = new TransparentButtonFeld("", new Position(EPositionIndex.Eins, EPositionIndex.Eins, EPositionIndex.Eins));
+		btnNewButton_43.addActionListener(Steuerung);
 		panel.add(btnNewButton_43);
 		
 		JLabel label_17 = new JLabel("");
@@ -208,6 +240,7 @@ public class Spielfeld2 extends JFrame {
 		panel.add(label_18);
 		
 		JButton btnNewButton_46 = new TransparentButtonFeld("", new Position(EPositionIndex.Eins, EPositionIndex.Zwei, EPositionIndex.Eins));
+		btnNewButton_46.addActionListener(Steuerung);
 		panel.add(btnNewButton_46);
 		
 		JLabel label_19 = new JLabel("");
@@ -217,6 +250,7 @@ public class Spielfeld2 extends JFrame {
 		panel.add(label_20);
 		
 		JButton btnNewButton_49 = new TransparentButtonFeld("", new Position(EPositionIndex.Eins, EPositionIndex.Drei, EPositionIndex.Eins));
+		btnNewButton_49.addActionListener(Steuerung);
 		panel.add(btnNewButton_49);
 	}
 		
---
+++ muehle_projekt/src/de/dhbw/gui/mActionListener.java
@@ -0,0 +1,15 @@
+package de.dhbw.gui;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class mActionListener implements ActionListener
+{
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
