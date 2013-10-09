cef1984e1189e1afc1fc1fbcad25cd8f015347d0
zyrus <getraenkeheini@web.de>
2013-10-09 11:06:55 +0200

    db change

--- muehle_projekt/src/de/dhbw/muehle_spiel/Database.java
+++ muehle_projekt/src/de/dhbw/muehle_spiel/Database.java
@@ -7,51 +7,30 @@ import java.io.*;
 
 public class Database {
 	public static void main(String[]args){
-		String driver="com.mysql.jdbc.Driver";
-		String url= "jdbc:mysql://localhost/Namen";
-		String user = "root";
-		String password="database";
+	String driver="org.sqlite.JDBC";
+	String url="jdbc:sqlite:test.db";
 		
-		try{
-			Class.forName(driver);
-			
-		}
-		catch(Exception e){
-			System.err.println(e.getMessage());
-			System.exit(0);
-		}
-	
-		Connection conn = null;
-		Statement hans= null;
-		try{
-			conn=DriverManager.getConnection("jdbc:mysql://localhost/Namen?user=root&password=database");
-			hans=conn.createStatement();
-			String query="SELECT * FROM Biersorten";
-			ResultSet result= hans.executeQuery(query);
-			if(result!=null){
-				ResultSetMetaData rsmd=result.getMetaData();
-				for(int i=1; i<=rsmd.getColumnCount();i++){
-					System.out.print(rsmd.getColumnName(i));
-					System.out.print((char)9);
-					
-				}
-				System.out.println();
-				
-				result.beforeFirst();
-				while(result.next()){
-					for(int column=1; column<=rsmd.getColumnCount();column++){
-						System.out.print(result.getObject(column));
-						System.out.print((char)9);
-						System.out.println();
-					}
-				}	result.close();
-			}
-		
-		}
-		catch(SQLException)
-	
+		    //Treiber laden
+		    try {
+		      Class.forName(driver);
+		      
+		    } catch ( Exception e ) {
+		    	System.err.println(e.getMessage());
+		        System.exit(0);
+		    }
+		    System.out.println("Opened database successfully");
+		    
+		    Connection c = null;
+		    c = DriverManager.getConnection(url);
 	}
-	
+
+
+public void createdb(){
 	
 }
 
+public void valuetrans(Position von, Position nach, ){
+	
+}
+
+}
