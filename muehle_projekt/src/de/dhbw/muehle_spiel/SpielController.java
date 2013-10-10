76ff60b03433e9099876127b21334cf6392f596b
georggr2 <georggr2@hotmail.com>
2013-10-10 23:27:28 +0200

    Spiel_Controller geändert, noch unvollständig.

--- muehle_projekt/src/de/dhbw/muehle_spiel/SpielController.java
+++ muehle_projekt/src/de/dhbw/muehle_spiel/SpielController.java
@@ -6,30 +6,47 @@ public class SpielController {
 	
 	public Spieler aktuellerSpieler;
 	
-	
-	
-	// Die zwei Spieler muessen erzeugt werden, damit das Spiel beginnen kann
-	
-	Spieler Spieler1 = new Spieler(ESpielsteinFarbe.SCHWARZ);
-	
-	Spieler Spieler2 = new Spieler(ESpielsteinFarbe.WEISS);
- 
+	int i=1;
 	
 	
 	
+	/*  Dieser Code in der GUI, um die Spieler zu erzeugen
+	*   Spieler Spieler1 = new Spieler(ESpielsteinFarbe.SCHWARZ);
+	*   Spieler Spieler2 = new Spieler(ESpielsteinFarbe.WEISS);
+    **/
 	
-	//Phase1 : Setzen der Steine
-	public void ablauf()
-	{
-		for (int i=1; i<=18; i++)
-		{ 
-			if ((i%2)==1) { aktuellerSpieler = Spieler1; }
-			
-			else { aktuellerSpieler=Spieler2;}
-			
-	}	
+	//TO DO: hol die 2 variablen Spieler 1 und 2
+	
+	
+	do {
+		if ((i%2)==1) { 
+			aktuellerSpieler = Spieler1; 
+			Ephase phase= new Ephase.getPhase();
+			//Spiel_beendet();
+		}
 		
-	}
+		else { 
+			aktuellerSpieler=Spieler2;
+		    Spieler2.getPhase(); 
+		  //Spiel_beendet();
+		    }
+		
+		i++;
+	   }
+	while (Spiel_beendet==false());
+		
+			
+			
+			
+			
+			
+		
+	
+	
+	
+	//Phase3: Einer oder beide Spieler haben 3 Steine(Unterscheidung?)
+	
+	
 	
 	/* In dieser Klasse werden nicht die Spieler erzeugt, das passiert alles in der GUI
 	 * Hier wird lediglich gesteuert welcher Spieler im Moment dran ist 
