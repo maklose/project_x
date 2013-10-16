3bb08b37fdf1db4c64cda2ddaf99e001d57dcdfe
georggr2 <georggr2@hotmail.com>
2013-10-16 11:08:48 +0200

    j

--- muehle_projekt/src/de/dhbw/muehle_spiel/SpielController.java
+++ muehle_projekt/src/de/dhbw/muehle_spiel/SpielController.java
@@ -8,36 +8,60 @@ import de.dhbw.muehle_spiel.EPhase;
 public class SpielController {
 	
 	Spieler aktuellerSpieler;
-	Spieler Spieler1;
-	Spieler Spieler2;
-	
-	//TO DO  Brauch ich einen Konstuktor? 
 	
 	
-	//TO DO: hol die 2 Objekte Spieler 1 und 2.. Aber wie?
+	//Konstruktor
+	public SpielController(Spieler s1, Spieler s2) 
+	{
+		Spieler Spieler1=s1;
+		Spieler Spieler2=s2; }
 	
+	
+	
+	
+	
+	public boolean Spiel_beendet(Spieler aktuellerSpieler){
+		if (aktuellerSpieler.getAnzahlSteine()<3) {return true; } //or if alle Steine gesperrt
+		
+		else {return false;}
+	}
+	
+	
+	
+	
+	
+	
+	public void Ablauf(){
+		
+		
+		
 	for (int i=1;;i++){
 	
 	if ((i%2)==1) {aktuellerSpieler=Spieler1; }
 	else {aktuellerSpieler=Spieler2; }
 		
-		
+		  if (Spiel_beendet(aktuellerSpieler)==false) {
 	
-		aktuellerSpieler.setAmZug(true); 
+		 
 		EPhase aktuellePhase= aktuellerSpieler.getPhase();
 		
-		/* Brauch ich diesen Code?
+		/* 
 		 * if (aktuellePhase==EPhase.Setzen){ aktuellerSpieler.setzeSpielstein(lPos);}
 		*else if(aktuellePhase==EPhase.Schieben){}
 		*else if (aktuellePhase==EPhase.Springen){}
 		**/
 		
-		//TO DO: Prüe ob eine Mühle entstanden ist, wenn ja dann nimm einen Stein weg
-	 // TO DO:Prüfe ob das Spiel beendet ist.
+		//TO DO: Prï¿½e ob eine Mï¿½hle entstanden ist, wenn ja dann nimm einen Stein weg
+	 
+		  }
+		  
+		  else if(Spiel_beendet(aktuellerSpieler)==true){
+			  //Spiel muss beendet werden, der akuelle Spiler ist der Verlierer!
+			
+		  }
 		
-		aktuellerSpieler.setAmZug(false);
 	}	
-	  
+	}
 		
 			
 			
