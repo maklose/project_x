3999f367a8967c4be17317ad3b31b9ab1e17e994
OrangeBlue <Orange_Blue_xx@web.de>
2013-10-16 12:15:31 +0200

    Anpassung Menü-GUI

--- muehle_projekt/src/de/dhbw/gui/Empfangsgui2.java
+++ muehle_projekt/src/de/dhbw/gui/Empfangsgui2.java
@@ -70,8 +70,8 @@ public class Empfangsgui2 extends JFrame {
 		panel.setBackground(new Color(255, 255, 255));
 		contentPane.add(panel, BorderLayout.CENTER);;;
 		GridBagLayout gbl_panel = new GridBagLayout();
-		gbl_panel.columnWidths = new int[]{0, 138, 0, 138, 0, 0};
-		gbl_panel.rowHeights = new int[]{150, 50, 20, 50, 0};
+		gbl_panel.columnWidths = new int[]{0, 138, 120, 138, 0, 0};
+		gbl_panel.rowHeights = new int[]{190, 50, 25, 50, 0};
 		gbl_panel.columnWeights = new double[]{1.0, 0.0, 1.0, 0.0, 1.0, Double.MIN_VALUE};
 		gbl_panel.rowWeights = new double[]{0.0, 0.0, 0.0, 0.0, Double.MIN_VALUE};
 		panel.setLayout(gbl_panel);
