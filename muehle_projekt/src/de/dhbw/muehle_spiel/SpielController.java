6bef7b7697c422fcc9ef59b569c999c2d8c905e7
georggr2 <georggr2@hotmail.com>
2013-10-16 12:01:06 +0200

    aa

--- muehle_projekt/src/de/dhbw/muehle_spiel/SpielController.java
+++ muehle_projekt/src/de/dhbw/muehle_spiel/SpielController.java
@@ -7,63 +7,35 @@ import de.dhbw.muehle_spiel.EPhase;
 
 public class SpielController {
 	
-	Spieler aktuellerSpieler;
-	
-	
-	//Konstruktor
-	public SpielController(Spieler s1, Spieler s2) 
-	{
-		Spieler Spieler1=s1;
-		Spieler Spieler2=s2; }
+	Spieler Spieler1;
+	Spieler Spieler2;
 	
 	
 	
-	
-	
-	public boolean Spiel_beendet(Spieler aktuellerSpieler){
-		if (aktuellerSpieler.getAnzahlSteine()<3) {return true; } //or if alle Steine gesperrt
+	if ((Spieler1.getAnzahlZuege()+Spieler2.getAnzahlZuege())%2==0){
 		
-		else {return false;}
+		SpielerAktiv=Spieler1;
+		SpielerPassiv=Spieler2;}
+	
+	else {
+		SpielerAktiv=Spieler2;
+	    SpielerPassiv=Spieler1;}
+	
+	 // if (checkSpielBeendet(SpielerAktiv, SpielerPassiv)==false)  ueberprueft ob das Spiel nicht beendet ist!
+	 // und dann kann der Aktive Spieler weitermachen
+	
+	SpielerAktiv.getPhase();
+	// je nach aktuelle Phase, die entsprechende Methode aufrufen
+	
+	//  else { Spiel ist beendet, der aktive Spieler hat verloren!  }
+	
+	
+	
+
+	
+	
 	}
 	
 	
 	
 	
-	
-	
-	public void Ablauf(){
-		
-		
-		
-	for (int i=1;;i++){
-	
-	if ((i%2)==1) {aktuellerSpieler=Spieler1; }
-	else {aktuellerSpieler=Spieler2; }
-		
-		  if (Spiel_beendet(aktuellerSpieler)==false) {
-	
-		 
-		EPhase aktuellePhase= aktuellerSpieler.getPhase();
-		
-		/* 
-		 * if (aktuellePhase==EPhase.Setzen){ aktuellerSpieler.setzeSpielstein(lPos);}
-		*else if(aktuellePhase==EPhase.Schieben){}
-		*else if (aktuellePhase==EPhase.Springen){}
-		**/
-		
-		//TO DO: Pr�e ob eine M�hle entstanden ist, wenn ja dann nimm einen Stein weg
-	 
-		  }
-		  
-		  else if(Spiel_beendet(aktuellerSpieler)==true){
-			  //Spiel muss beendet werden, der akuelle Spiler ist der Verlierer!
-			
-		  }
-		
-	}	
-	}
-		
-			
-			
-}
-			
