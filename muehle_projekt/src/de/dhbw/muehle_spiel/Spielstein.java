ba54bdf32e765fe08c277eba452de6a5c8a33577
Philipp <philippgerds@gmail.com>
2013-10-16 10:52:16 +0200

    ...

--- muehle_projekt/src/de/dhbw/muehle_spiel/Spielstein.java
+++ muehle_projekt/src/de/dhbw/muehle_spiel/Spielstein.java
@@ -2,16 +2,12 @@
 
 package de.dhbw.muehle_spiel;
 import de.dhbw.muehle_api.*;
-import de.dhbw.muehle_api.strategy.IBewegung;
-import de.dhbw.muehle_api.strategy.ISpielzug;
 
-public class Spielstein implements ISpielstein, IBewegung
+public class Spielstein implements ISpielstein
 {
 	//Variablen
 	private ESpielsteinFarbe Spielsteinfarbe; 			//Die Farbe des Spielsteins
-	private boolean gesperrt;							//zeigt an ob ein Stein gelöscht werden darf (wenn er z.b. in einer Mühle steht
 	private Position aktuellePosition;					//hat die aktuelle Position des Steines
-	private boolean ausgewählt; 						//true wenn der Stein ausgewählt wurde
 	private int xPos, yPos;								//Die echte Position die ein Spielstein auf dem Feld hat
 	
 	//neuen Spieltein erzeugen
@@ -19,8 +15,6 @@ public class Spielstein implements ISpielstein, IBewegung
 	{
 		Spielsteinfarbe = lSpielsteinfarbe;
 		aktuellePosition = laktuellePosition; 
-		gesperrt = false;
-		ausgewählt = false;
 		xPos = lxPos;
 		yPos = lyPos;
 
@@ -44,9 +38,7 @@ public class Spielstein implements ISpielstein, IBewegung
 	public int getyPos() {
 		return yPos;
 	}
-
-	
-	
+		
 	//hiermit kann man einen Spielstein bewegen 
 	public void bewegen(Bewegung neueBewegung, int lxPos, int lyPos)
 	{
@@ -55,78 +47,22 @@ public class Spielstein implements ISpielstein, IBewegung
 		yPos = lyPos;
 
 	}
-	
-	public ESpielsteinFarbe getSpielsteinfarbe() 
-	{
-		return Spielsteinfarbe;
-	}
-
-	public void setSpielsteinfarbe(ESpielsteinFarbe spielsteinfarbe) 
-	{
-		Spielsteinfarbe = spielsteinfarbe;
-	}
-
-	public boolean isGesperrt() 
-	{
-		return gesperrt;
-	}
-
-	public void setGesperrt(boolean gesperrt) 
-	{
-		this.gesperrt = gesperrt;
-	}
-
-	public Position getAktuellePosition() 
-	{
-		return aktuellePosition;
-	}
-
-	public void setAktuellePosition(Position akutellePosition) 
-	{
-		this.aktuellePosition = akutellePosition;
-	}
-
-	public boolean isAusgewählt() 
-	{
-		return ausgewählt;
-	}
-
-	public void setAusgewählt(boolean ausgewählt) 
-	{
-		this.ausgewählt = ausgewählt;
-	}
-
-	@Override
-	public Position altePosition() 
-	{
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public Position neuePosition() 
-	{
-		// TODO Auto-generated method stub
-		return null;
-	}
 
 	@Override
 	public ESpielsteinFarbe getFarbe() 
 	{
-		// TODO Auto-generated method stub
-		return null;
+		return Spielsteinfarbe;
 	}
 
 	@Override
 	public Position getPosition() 
 	{
-		// TODO Auto-generated method stub
-		return null;
+		return aktuellePosition;
 	}
 	
 	public boolean FarbVergleich(ESpielsteinFarbe b)
 	{
-		if(this.getSpielsteinfarbe() == b)
+		if(this.getFarbe() == b)
 			return true;
 		else
 			return false;
