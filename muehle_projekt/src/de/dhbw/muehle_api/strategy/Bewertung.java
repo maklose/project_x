78f314d19395da8aaaa227a773f5e66e937005c9
georggr2 <georggr2@hotmail.com>
2013-10-21 12:29:04 +0200

    Bewertung

--- muehle_projekt/src/de/dhbw/muehle_api/strategy/Bewertung.java
+++ muehle_projekt/src/de/dhbw/muehle_api/strategy/Bewertung.java
@@ -1,5 +1,35 @@
 package de.dhbw.muehle_api.strategy;
 
-public class Bewertung {
+public class Bewertung {                     //Final Klasse?
 
-}
+	
+
+	    
+	    public static final int WIN = 5000000;
+
+	 
+	     
+	    public static final int MILL = 10000;
+
+	     
+	     public static final int VERYHIGH = 1000;
+	        
+	     
+	     public static final int HIGH = 500;
+
+	     
+	  
+	     public static final int MEDIUM = 100;
+	       
+	     
+	     
+	     public static final int LOW = 40;
+	       
+	     
+	     public static final int VERYLOW = 10;
+	     
+	     
+	     public static final int ZERO = 0;
+	}
+	
+
