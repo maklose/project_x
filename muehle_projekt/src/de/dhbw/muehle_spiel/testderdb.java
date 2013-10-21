5ee707dd5cffd7fc2f07c113bf4e594ec69ba7df
maklose <marvin.klose92@gmail.com>
2013-10-21 14:11:00 +0200

    ads

--- muehle_projekt/src/de/dhbw/muehle_spiel/testderdb.java
+++ muehle_projekt/src/de/dhbw/muehle_spiel/testderdb.java
@@ -8,7 +8,7 @@ public class testderdb {
 			//db.erzeuge_p();
 			//db.zugspeichern();
 			//db.zeige_p();
-			//db.lösche_p();
+			db.lösche_p();
 			//db.erzeuge_h();
 			//db.lösche_h();
 			//db.anzahlzuegespeichern();
