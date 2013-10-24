1fc5df270dd7c1a6272d77a201fee51677ff74d6
Philipp <philippgerds@gmail.com>
2013-10-24 20:43:43 +0200

    MinMax Algorithmus in Streategie

--- muehle_projekt/src/de/dhbw/strategy/Strategie.java
+++ muehle_projekt/src/de/dhbw/strategy/Strategie.java
@@ -41,18 +41,25 @@ Bewegung bewegung;
 
 	@Override
 	public ISpielzug bewegeStein(List<ISpielstein> p_SpielFeld) throws StrategieException {
+     
+		double bewertung;
+		Spielzug zug = null;
 		
 		if(anzahlZuege < 9){
-		
-		}
-		Spielzug zug = new Spielzug(bewegung);	
+		bewertung = max(tiefe, p_SpielFeld, null, anzahlZuege);
 		anzahlZuege++;
-		
+		zug = new Spielzug(bewegung);
+		}
+		else{
+		bewertung = max(tiefe, p_SpielFeld, null, anzahlZuege );
+		anzahlZuege++;	
+		zug = new Spielzug(new Spielstein(farbe, bewegung.getNach(), 0, 0, 0));
+		}
 		return zug;
 	}
 
 	//Liefert alle möglichen Bewegungnen eines Spielsteins
-	public List<Bewegung> getMoeglicheBewegungen(List<ISpielstein> p_SpielFeld, Spielstein lStein){
+	public List<Bewegung> getMoeglicheBewegungen(List<ISpielstein> p_SpielFeld, Spielstein lStein, int lanzahlZuege){
 	
 	List<Bewegung> moeglZuege = new ArrayList<Bewegung>();
 	
@@ -85,6 +92,45 @@ Bewegung bewegung;
 	
 	Pruefung pruef = new Pruefung();
 	
+	// alle Mögliche Positionen, auf die ein Stein gesetzt werden darf
+	if(lanzahlZuege < 9){
+		for( int i = 0; i< 3; i++){
+			if(i == 0)
+				ebene = ebene.Eins;
+			if(i == 1)
+				ebene = ebene.Zwei;
+			if(i == 2)
+				ebene = ebene.Drei;
+		for(int j = 0; j < 3; j++){
+				if(j == 0)
+					x = x.Eins;
+				if(j == 1)
+					x = x.Zwei;
+				if(j == 2)
+					x = x.Drei;
+			for(int k = 0; k < 3; k++){
+					if(k == 0)
+						y = y.Eins;
+					if(k == 1)
+						y = y.Zwei;
+					if(k == 2)
+						y = y.Drei;
+					
+					if((ebene == ebene.Eins && x == x.Zwei && y == y.Zwei) 
+							|| (ebene == ebene.Zwei && x == x.Zwei && y == y.Zwei)
+							|| (ebene == ebene.Drei && x == x.Zwei && y == y.Zwei))
+						continue;
+					
+					if(pruef.checkSetzen(new Position(ebene, x, y), Spieler1, Spieler2) == true)
+					{
+						moeglZuege.add(new Bewegung(null, new Position(ebene, x, y)));
+					}
+				}
+			}	
+		}		
+	}
+		
+	else{	
 	//Überprüfung für jede mögl. Position auf dem Brett, ob der Stein dahin/springen ziehen darf	
 		for( int i = 0; i< 3; i++){
 				if(i == 0)
@@ -120,18 +166,12 @@ Bewegung bewegung;
 				}
 			}	
 		}		
+	}	
 		return moeglZuege;
 	}
 	
-	// Liefert die Bewertung eines Spielzugs
-	private double Bewertung(List<ISpielstein> p_SpielFeld, Bewegung bewegung){
-		double bewertung = 0;
-		
-		return bewertung;
-	}
 	
-	
-	private double max (int ltiefe, List<ISpielstein> p_SpielFeld, Bewegung bewegung){	
+	private double max (int ltiefe, List<ISpielstein> p_SpielFeld, Bewegung bewegung, int lanzahlZuege){	
 			
 	ESpielsteinFarbe farbeSpieler2;
 	
@@ -148,10 +188,15 @@ Bewegung bewegung;
 	//neues Spielfeld erzeugen
 	List<ISpielstein> neuesSpielFeld = p_SpielFeld;
 	int zaehler = 0;
+	int index = 0; 		//Index des ISpielstein der bewegt wurde
 	for(ISpielstein s : neuesSpielFeld){
 		zaehler++;
-		if(s.getPosition() == bewegung.getNach()){
-			neuesSpielFeld.set(zaehler, new Spielstein(farbe, bewegung.getVon(), 0,0,0));
+		if(bewegung.getVon()== null){
+		neuesSpielFeld.add(new Spielstein(spieler1.getSpielerfarbe(), bewegung.getVon(), 0,0,0));	
+		}			
+		else if(s.getPosition() == bewegung.getVon()){
+			index = zaehler;
+			neuesSpielFeld.set(zaehler, new Spielstein(farbe, bewegung.getNach(), 0,0,0));
 		}			
 	}
 	
@@ -170,9 +215,11 @@ Bewegung bewegung;
 	
 	Pruefung pruef = new Pruefung();
 	
+	// Wenn die tiefe 0 ist oder das Spielbeendet ist, wird die Beweertung des Spielzugs geholt
 	if(tiefe == 0 || pruef.checkSpielBeendet(spieler1, spieler2) == true)
 	{
-	return Bewertung(p_SpielFeld, bewegung);
+	Bewertung bewertung = new Bewertung();	
+	return bewertung.bewerteZug(p_SpielFeld, bewegung, index, lanzahlZuege);
 	}	
 		
 	List<Bewegung> moeglBewegungen = new ArrayList<Bewegung>();
@@ -180,25 +227,41 @@ Bewegung bewegung;
 	double maxWert= -1;
 	double wert;
 	
+	if(lanzahlZuege >= 9){ // ziehen und springen Phase
+	
 	for(int i = 0; i < spieler1.getAnzahlSteine(); i++){	
 	
-		moeglBewegungen = getMoeglicheBewegungen(neuesSpielFeld, spieler1.Steine[i]);
+		moeglBewegungen = getMoeglicheBewegungen(neuesSpielFeld, spieler1.Steine[i], lanzahlZuege);
 		for(int j = 0; j < moeglBewegungen.size();j++){	
 			//Rekursion
-			wert= min((ltiefe - 1) , neuesSpielFeld, moeglBewegungen.get(j));
+			wert= min((ltiefe - 1) , neuesSpielFeld, moeglBewegungen.get(j), (lanzahlZuege + 1));
 			if( wert > maxWert){
-				wert = min((ltiefe - 1) ,neuesSpielFeld, bewegung);	
+				maxWert = wert;	
 				if(ltiefe == tiefe)
 				bewegung = moeglBewegungen.get(j); 	 
 			}
 		}	
+	}
+	}	
+	else  //setzen Phase
+	{
+		moeglBewegungen = getMoeglicheBewegungen(neuesSpielFeld, new Spielstein(farbe, null, 0,0,0), lanzahlZuege);
+		for(int j = 0; j < moeglBewegungen.size();j++){	
+			//Rekursion
+			wert= min((ltiefe - 1) , neuesSpielFeld, moeglBewegungen.get(j), (lanzahlZuege + 1));
+			if( wert > maxWert){
+				maxWert = wert;	
+				if(ltiefe == tiefe)
+				bewegung = moeglBewegungen.get(j);			
+			}
+		}
 	}
 	
 	return maxWert;
 	}
 		
 	
-	private double min (int ltiefe, List<ISpielstein> p_SpielFeld, Bewegung bewegung){
+	private double min (int ltiefe, List<ISpielstein> p_SpielFeld, Bewegung bewegung, int lanzahlZuege){
 		
 		ESpielsteinFarbe farbeSpieler2;
 		
@@ -215,10 +278,15 @@ Bewegung bewegung;
 		//neues Spielfeld erzeugen
 		List<ISpielstein> neuesSpielFeld = p_SpielFeld;
 		int zaehler = 0;
+		int index = 0; 		//Index des ISpielstein der bewegt wurde
 		for(ISpielstein s : neuesSpielFeld){
 			zaehler++;
-			if(s.getPosition() == bewegung.getNach()){
-				neuesSpielFeld.set(zaehler, new Spielstein(farbe, bewegung.getVon(), 0,0,0));
+			if(bewegung.getVon()== null){
+				neuesSpielFeld.add(new Spielstein(spieler2.getSpielerfarbe(), bewegung.getVon(), 0,0,0));	
+				}			
+			else if(s.getPosition() == bewegung.getVon()){
+				index = zaehler;
+				neuesSpielFeld.set(zaehler, new Spielstein(farbe, bewegung.getNach(), 0,0,0));
 			}			
 		}
 		
@@ -237,9 +305,11 @@ Bewegung bewegung;
 		
 		Pruefung pruef = new Pruefung();
 		
+		// Wenn die tiefe 0 ist oder das Spielbeendet ist, wird die Beweertung des Spielzugs geholt
 		if(tiefe == 0 || pruef.checkSpielBeendet(spieler2, spieler1) == true)
 		{
-		return Bewertung(p_SpielFeld, bewegung);
+		Bewertung bewertung = new Bewertung();	
+		return bewertung.bewerteZug(p_SpielFeld, bewegung, index, lanzahlZuege);
 		}	
 			
 		List<Bewegung> moeglBewegungen = new ArrayList<Bewegung>();
@@ -249,12 +319,12 @@ Bewegung bewegung;
 		
 		for(int i = 0; i < spieler2.getAnzahlSteine(); i++){	
 		
-			moeglBewegungen = getMoeglicheBewegungen(neuesSpielFeld, spieler2.Steine[i]);
+			moeglBewegungen = getMoeglicheBewegungen(neuesSpielFeld, spieler2.Steine[i], lanzahlZuege);
 			for(int j = 0; j < moeglBewegungen.size();j++){	
 				//Rekursion
-				wert= min((ltiefe - 1) , neuesSpielFeld, moeglBewegungen.get(j));
+				wert= max((ltiefe - 1) , neuesSpielFeld, moeglBewegungen.get(j), (lanzahlZuege + 1));
 				if( wert < minWert){
-					wert = max((ltiefe - 1) ,neuesSpielFeld, moeglBewegungen.get(j));	
+					minWert = wert;	
 					if(ltiefe == tiefe)
 					bewegung = moeglBewegungen.get(j); 	 
 				}
