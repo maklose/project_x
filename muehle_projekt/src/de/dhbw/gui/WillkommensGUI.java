156753295d793bc208fb4536fbdc08c691c71d96
maklose <marvin.klose92@gmail.com>
2013-10-25 13:24:06 +0200

    asd

--- muehle_projekt/src/de/dhbw/gui/WillkommensGUI.java
+++ muehle_projekt/src/de/dhbw/gui/WillkommensGUI.java
@@ -5,6 +5,7 @@ import java.awt.EventQueue;
 import java.awt.Graphics;
 import java.awt.Image;
 import java.awt.Toolkit;
+import java.awt.image.ImageObserver;
 
 import javax.swing.JFrame;
 import javax.swing.JPanel;
@@ -73,14 +74,49 @@ public class WillkommensGUI extends JFrame {
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
 		contentPane.setLayout(new BorderLayout(0, 0));
+
 		
-		JPanel panel = new JPanel(){  
+		final JPanel panel = new JPanel(){  
             public void paintComponent(Graphics g) {  
-                Image hintergrund = Toolkit.getDefaultToolkit().getImage(  
+                final Image hintergrund = Toolkit.getDefaultToolkit().getImage(  
                           Empfangsgui2.class.getResource("/de/dhbw/images/Willkommen_GUI.PNG"));  
+                final Image ladebalken = Toolkit.getDefaultToolkit().getImage(  
+                        Empfangsgui2.class.getResource("/de/dhbw/images/Ladebalken.PNG"));  
+                
             g.drawImage(hintergrund, 0, 0, this.getWidth(), this.getHeight(), this);  
-       }}  ;
+            
+            final ImageObserver observer = this;
+            final Graphics grafik = g;
+//            grafik.drawImage(hintergrund, 10, 200,200 ,200 , observer);
+            new Thread() 
+			{
+				{
+					start(); 
+				}
+				public void run()
+				{
+					try 
+					{ 
+						 for(int i = 0; i < 100; i++)
+			            {
+			            	grafik.drawImage(hintergrund, 10 + i*8, 200,200 ,20 , observer);		//REPAINT!!
+			            	sleep(100);
+			            }
+							
+						
+					}
+					catch ( InterruptedException e ) 
+					{ 
+						e.printStackTrace();
+					} 
+				} 
+			};  //Thread zum warten
+            
+           
+            
+       }};
 		contentPane.add(panel, BorderLayout.CENTER);
 	}
-
+	
+	
 }
