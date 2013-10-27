58f0e9791c9a21fa19afe9c0a2fb14e924a86603
Philipp <philippgerds@gmail.com>
2013-10-27 18:05:23 +0100

    kk

--- muehle_projekt/src/de/dhbw/strategy/Strategie.java
+++ muehle_projekt/src/de/dhbw/strategy/Strategie.java
@@ -19,8 +19,8 @@ public class Strategie implements IStrategie{
 
 int anzahlZuege;
 ESpielsteinFarbe farbe;
-ESpielsteinFarbe farbeSpieler2;
 int tiefe = 3;
+
 private Bewegung bewegung;
 
 List<ISpielstein> SpielFeld;
@@ -77,78 +77,40 @@ List<ISpielstein> SpielFeld;
 	//Liefert alle möglichen Bewegungnen eines Spielsteins
 	public List<Bewegung> getMoeglicheBewegungen(List<ISpielstein> p_SpielFeld, Spielstein lStein, int lanzahlZuege){
 	
-	List<Bewegung> moeglZuege = new ArrayList<Bewegung>();
-	
-	ESpielsteinFarbe farbeSpieler2;
-	
-	if( lStein.getFarbe() == ESpielsteinFarbe.SCHWARZ){
-		farbeSpieler2 = ESpielsteinFarbe.WEISS;
-	}
-	else{
-		farbeSpieler2 = ESpielsteinFarbe.SCHWARZ;
-	}
+		List<Bewegung> moeglZuege = new ArrayList<Bewegung>();
 		
-	Spieler Spieler1 = new Spieler(lStein.getFarbe(), "");
-	Spieler Spieler2 = new Spieler(farbeSpieler2, "");
-	
-	//Erzeugung der Spielsteine, die in p_Spielfeld vorhanden sind
-	for(ISpielstein s: p_SpielFeld){
-		if(s.getFarbe() == lStein.getFarbe())
-		{
-			Spieler1.setzeSpielstein(s.getPosition(), 0, 0);
+		ESpielsteinFarbe farbeSpieler2;
+		
+		if( lStein.getFarbe() == ESpielsteinFarbe.SCHWARZ){
+			farbeSpieler2 = ESpielsteinFarbe.WEISS;
 		}
 		else{
-			Spieler2.setzeSpielstein(s.getPosition(), 0, 0);
+			farbeSpieler2 = ESpielsteinFarbe.SCHWARZ;
 		}
-	}
+			
+		Spieler Spieler1 = new Spieler(lStein.getFarbe(), "");
+		Spieler Spieler2 = new Spieler(farbeSpieler2, "");
 		
-	EPositionIndex ebene = null;
-	EPositionIndex x = null;
-	EPositionIndex y = null;
-	
-	Pruefung pruef = new Pruefung();
-	
-	// alle Mögliche Positionen, auf die ein Stein gesetzt werden darf
-	if(lanzahlZuege <= 18){
-		for( int i = 0; i< 3; i++){
-			if(i == 0)
-				ebene = ebene.Eins;
-			if(i == 1)
-				ebene = ebene.Zwei;
-			if(i == 2)
-				ebene = ebene.Drei;
-		for(int j = 0; j < 3; j++){
-				if(j == 0)
-					x = x.Eins;
-				if(j == 1)
-					x = x.Zwei;
-				if(j == 2)
-					x = x.Drei;
-			for(int k = 0; k < 3; k++){
-					if(k == 0)
-						y = y.Eins;
-					if(k == 1)
-						y = y.Zwei;
-					if(k == 2)
-						y = y.Drei;
-					
-					if((ebene == ebene.Eins && x == x.Zwei && y == y.Zwei) 
-							|| (ebene == ebene.Zwei && x == x.Zwei && y == y.Zwei)
-							|| (ebene == ebene.Drei && x == x.Zwei && y == y.Zwei))
-						continue;
-					
-					if(pruef.checkSetzen(new Position(ebene, x, y), Spieler1, Spieler2) == true)
-					{
-						moeglZuege.add(new Bewegung(null, new Position(ebene, x, y)));
-					}
-				}
-			}	
-		}		
-	}
+		//Erzeugung der Spielsteine, die in p_Spielfeld vorhanden sind
+		for(ISpielstein s: p_SpielFeld){
+			if(s.getFarbe() == lStein.getFarbe())
+			{
+				Spieler1.setzeSpielstein(s.getPosition(), 0, 0);
+			}
+			else{
+				Spieler2.setzeSpielstein(s.getPosition(), 0, 0);
+			}
+		}
+			
+		EPositionIndex ebene = null;
+		EPositionIndex x = null;
+		EPositionIndex y = null;
 		
-	else{	
-	//Überprüfung für jede mögl. Position auf dem Brett, ob der Stein dahin/springen ziehen darf	
-		for( int i = 0; i< 3; i++){
+		Pruefung pruef = new Pruefung();
+		
+		// alle Mögliche Positionen, auf die ein Stein gesetzt werden darf
+		if(lanzahlZuege <= 18){
+			for( int i = 0; i< 3; i++){
 				if(i == 0)
 					ebene = ebene.Eins;
 				if(i == 1)
@@ -175,16 +137,54 @@ List<ISpielstein> SpielFeld;
 								|| (ebene == ebene.Drei && x == x.Zwei && y == y.Zwei))
 							continue;
 						
-						if(pruef.checkZug(new Bewegung(lStein.getPosition(), new Position(ebene, x, y)), Spieler1, Spieler2) == true)
+						if(pruef.checkSetzen(new Position(ebene, x, y), Spieler1, Spieler2) == true)
 						{
-							moeglZuege.add(new Bewegung(lStein.getPosition(), new Position(ebene, x, y)));
+							moeglZuege.add(new Bewegung(null, new Position(ebene, x, y)));
 						}
-				}
-			}	
-		}		
-	}	
-		return moeglZuege;
-	}
+					}
+				}	
+			}		
+		}
+			
+		else{	
+		//Überprüfung für jede mögl. Position auf dem Brett, ob der Stein dahin/springen ziehen darf	
+			for( int i = 0; i< 3; i++){
+					if(i == 0)
+						ebene = ebene.Eins;
+					if(i == 1)
+						ebene = ebene.Zwei;
+					if(i == 2)
+						ebene = ebene.Drei;
+				for(int j = 0; j < 3; j++){
+						if(j == 0)
+							x = x.Eins;
+						if(j == 1)
+							x = x.Zwei;
+						if(j == 2)
+							x = x.Drei;
+					for(int k = 0; k < 3; k++){
+							if(k == 0)
+								y = y.Eins;
+							if(k == 1)
+								y = y.Zwei;
+							if(k == 2)
+								y = y.Drei;
+							
+							if((ebene == ebene.Eins && x == x.Zwei && y == y.Zwei) 
+									|| (ebene == ebene.Zwei && x == x.Zwei && y == y.Zwei)
+									|| (ebene == ebene.Drei && x == x.Zwei && y == y.Zwei))
+								continue;
+							
+							if(pruef.checkZug(new Bewegung(lStein.getPosition(), new Position(ebene, x, y)), Spieler1, Spieler2) == true)
+							{
+								moeglZuege.add(new Bewegung(lStein.getPosition(), new Position(ebene, x, y)));
+							}
+					}
+				}	
+			}		
+		}	
+			return moeglZuege;
+		}
 	
 	
 	private double max (int ltiefe, Bewegung bewegung, int lanzahlZuege){	
@@ -204,7 +204,7 @@ List<ISpielstein> SpielFeld;
 	if(ltiefe != 0){
 		if((lanzahlZuege - anzahlZuege) > 0 ){
 			//ISpielstein an  Feld anfügen, wenn Stein neu auf das Feld gesetzt wurde
-			if(lanzahlZuege < 18){
+			if(lanzahlZuege <= 18){
 			SpielFeld.add(new Spielstein(spieler2.getSpielerfarbe(), bewegung.getNach(), 0,0,0));	
 			}
 		
@@ -319,7 +319,7 @@ List<ISpielstein> SpielFeld;
 	
 	if(ltiefe != 0){
 		//ISpielstein an  Feld anfügen, wenn Stein neu gesetzt wurde
-		if(lanzahlZuege < 18){
+		if(lanzahlZuege <= 18){
 			SpielFeld.add(new Spielstein(spieler1.getSpielerfarbe(), bewegung.getNach(), 0,0,0));	
 			}
 	
@@ -422,9 +422,127 @@ List<ISpielstein> SpielFeld;
 	
 	@Override
 	public ISpielstein entferneStein(List<ISpielstein> p_SpielFeld, ISpielzug p_LetzterSpielzug) throws StrategieException {
-		// TODO Auto-generated method stub
-		return null;
+		
+	ESpielsteinFarbe farbeSpieler2;
+	boolean aufLinie;
+	List<Spielstein> SteineAufLinie = new ArrayList();
+	Pruefung pruef = new Pruefung();
+		
+	//Festlegen der Spielerfarben
+	if( farbe == ESpielsteinFarbe.SCHWARZ)
+		farbeSpieler2 = ESpielsteinFarbe.WEISS;
+	else
+		farbeSpieler2 = ESpielsteinFarbe.SCHWARZ;
+			
+	Spieler Spieler1  = new Spieler(farbe, "");
+	Spieler Spieler2  = new Spieler(farbeSpieler2, "");
+	
+	//Ablegen der Steine aus P_SpielFeld in den Spielern
+	for(ISpielstein s: p_SpielFeld){
+		if(s.getFarbe() == farbe)
+		{
+			Spieler1.setzeSpielstein(s.getPosition(), 0, 0);
+		}
+		else
+		{
+			Spieler2.setzeSpielstein(s.getPosition(),0,0);
+		}
 	}
 	
+	//Überprüfen, ob zwei Steine der selben Farbe auf einer Linie stehen
+	EPositionIndex ebene = null;
+	EPositionIndex x = null;
+	EPositionIndex y = null;
+	
+	int aenderungEbene, aenderungX, aenderungY;
+	int zaehlerEbene = 0;
+	int zaehlerX = 0; 
+	int zaehlerY = 0;
+	
+	Spielstein[] Steine = new Spielstein[Spieler2.getAnzahlSteine()];
+	
+	for (int index = 0; index < Spieler2.getAnzahlSteine(); index++)
+	{
+		Steine[index] = Spieler2.Steine[index];
+	}
+	
+	int[][] Positionen = new int[9][3];
+	
+	// Ablegen der Positionen aller Steine eines Spielers in einem Array
+	for (int i = 0; i < 9; i++){
+		if(Steine[i] != null){
+			Positionen[i][0]= Steine[i].getPosition().getEbene().getValue();
+			Positionen[i][1]= Steine[i].getPosition().getX().getValue();
+			Positionen[i][2]= Steine[i].getPosition().getY().getValue();
+			}
+	}
+	
+	for(int IndexStein = 0; IndexStein < Spieler2.getAnzahlSteine(); IndexStein++){
+		for (int j =0; j <9; j++){
+			
+			aenderungEbene = 0;
+			aenderungX = 0;
+			aenderungY = 0;
+			
+			//Vergleicht die Position des betrachteten Steins(IndexStein) mit der des Steins an j-ter Stelle
+			aenderungEbene = Math.abs(Positionen[j][0] - Positionen[IndexStein][0]);
+			aenderungX = Math.abs(Positionen[j][1] - Positionen[IndexStein][1]);
+			aenderungY = Math.abs(Positionen[j][2] - Positionen[IndexStein][2]);
+					
+			//Heraufsetzen des Zählers um 1, wenn sich nur die Ebene verändert hat
+			if ((aenderungEbene == 1 && aenderungX == 0 && aenderungY == 0)||
+				(aenderungEbene == 2 && aenderungX == 0 && aenderungY == 0))
+			{
+				zaehlerEbene ++;
+				
+				
+				// Ausschließen der Eckpositionen
+				//Sonderfälle: 1,1,3 - 2,1,3 - 3,1,3 ; 1,3,3 - 2,3,3 - 3,3,3 ; 1,3,1 - 2,3,1 - 3,3,1 ; 1,1,1 - 2,1,1 - 3,1,1
+				if	((Positionen[j][0] != Positionen[IndexStein][0])&&
+								((Positionen[j][1] + Positionen[j][2] == 2)||
+								(Positionen[j][1] + Positionen[j][2] == 4)||
+								(Positionen[j][1] + Positionen[j][2] == 6)))
+				{
+					zaehlerEbene--;
+				}
+											
+			}	
+			
+			//Heraufsetzen des Zählers um 1, wenn sich nur die X-Koordinate verändert hat
+			if ((aenderungEbene == 0 && aenderungX == 1 && aenderungY == 0)||
+				(aenderungEbene == 0 && aenderungX == 2 && aenderungY == 0))
+				{
+					zaehlerX ++;
+				}
+			
+			//Heraufsetzen des Zählers um 1, wenn sich nur die Y-Koordinate verändert hat
+			if ((aenderungEbene == 0 && aenderungX == 0 && aenderungY == 1)||
+				(aenderungEbene == 0 && aenderungX == 0 && aenderungY == 2))
+					{
+						zaehlerY ++;
+					}
+			
+		}
+	
+		if(zaehlerEbene == 1 || zaehlerX == 1 || zaehlerY == 1){
+				aufLinie = true;
+				
+				//Abrfagen, ob der Stein[IndexStein in einer Mühle steht]
+				// wenn nicht hinzufügen zu SteineAufLinie
+				if(pruef.checkInMuehle(IndexStein, Spieler2.Steine) == false)
+				SteineAufLinie.add(Spieler2.Steine[IndexStein]);		
+			}
+		
+		}
+	
+	
+	//Stein auswählen, der entfernt werden soll
+	if(SteineAufLinie.size() >0){
+		int index = (int)Math.random()
+		SteineAufLinie.get(Math.random());
+	}
+	
+	}	
+			
 }
 
