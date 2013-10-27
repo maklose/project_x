21a22ba70f8ceefad7e5fc941af95504a3873691
OrangeBlue <Orange_Blue_xx@web.de>
2013-10-27 20:45:11 +0100

    Anpassung Frühling

--- muehle_projekt/src/de/dhbw/gui/Spielfeld.java
+++ muehle_projekt/src/de/dhbw/gui/Spielfeld.java
@@ -450,7 +450,7 @@ public class Spielfeld extends JFrame implements ActionListener {
 					case 1: spielfeld = Toolkit.getDefaultToolkit().getImage(  
 	                          Spielfeld.class.getResource("/de/dhbw/images/Spielbrett_GUIlinks.png")); break;
 					case 2: spielfeld = Toolkit.getDefaultToolkit().getImage(  
-	                          Spielfeld.class.getResource("/de/dhbw/images/Spielbrett_Frühling_GUIlinks.png")); break;
+	                          Spielfeld.class.getResource("/de/dhbw/images/Spielbrett_Frühling_GUI_links.png")); break;
 					case 3: spielfeld = Toolkit.getDefaultToolkit().getImage(  
 	                          Spielfeld.class.getResource("/de/dhbw/images/Spielbrett_GUIlinks.png")); break;
 					case 4: spielfeld = Toolkit.getDefaultToolkit().getImage(  
@@ -783,7 +783,7 @@ public class Spielfeld extends JFrame implements ActionListener {
 					case 1: spielfeldRechts = Toolkit.getDefaultToolkit().getImage(  
 	                          Spielfeld.class.getResource("/de/dhbw/images/Spielbrett_GUIrechts.png")); break;
 					case 2: spielfeldRechts = Toolkit.getDefaultToolkit().getImage(  
-	                          Spielfeld.class.getResource("/de/dhbw/images/Spielbrett_Frühling_GUIrechts.png")); break;
+	                          Spielfeld.class.getResource("/de/dhbw/images/Spielbrett_Frühling_GUI_rechts.png")); break;
 					case 3: spielfeldRechts = Toolkit.getDefaultToolkit().getImage(  
 	                          Spielfeld.class.getResource("/de/dhbw/images/Spielbrett_GUIrechts.png")); break;
 					case 4: spielfeldRechts = Toolkit.getDefaultToolkit().getImage(  
