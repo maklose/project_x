e0bc649dea3d43656f19ff929e182e489cad5a1f
akinna <delphinidae12@gmail.com>
2013-10-27 11:06:27 +0100

    Spielverlauf Tabelle

--- muehle_projekt/src/de/dhbw/gui/Spielverlauf.java
+++ muehle_projekt/src/de/dhbw/gui/Spielverlauf.java
@@ -6,10 +6,14 @@ import java.awt.EventQueue;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+import javax.swing.JScrollPane;
+import javax.swing.JScrollBar;
+import javax.swing.JTable;
+import javax.swing.BoxLayout;
+import javax.swing.table.DefaultTableModel;
 
 public class Spielverlauf extends JFrame {
-
-	private JPanel contentPane;
+	private JTable table;
 
 	/**
 	 * Launch the application.
@@ -32,11 +36,42 @@ public class Spielverlauf extends JFrame {
 	 */
 	public Spielverlauf() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 450, 300);
-		contentPane = new JPanel();
-		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-		contentPane.setLayout(new BorderLayout(0, 0));
-		setContentPane(contentPane);
+		setBounds(100, 100, 550, 300);
+		getContentPane().setLayout(new BorderLayout(0, 0));
+		
+		JScrollPane scrollPane = new JScrollPane();
+		getContentPane().add(scrollPane, BorderLayout.CENTER);
+		
+		JScrollBar scrollbar = new JScrollBar ();
+		scrollPane.add(scrollbar);
+		
+		table = new JTable();
+		table.setModel(new DefaultTableModel(
+			new Object[][] {
+				{null, null, null, null, null, null, null, null, null},
+				{null, null, null, null, null, null, null, null, null},
+				{null, null, null, null, null, null, null, null, null},
+				{null, null, null, null, null, null, null, null, null},
+				{null, null, null, null, null, null, null, null, null},
+				{null, null, null, null, null, null, null, null, null},
+				{null, null, null, null, null, null, null, null, null},
+				{null, null, null, null, null, null, null, null, null},
+				{null, null, null, null, null, null, null, null, null},
+				{null, null, null, null, null, null, null, null, null},
+				{null, null, null, null, null, null, null, null, null},
+				{null, null, null, null, null, null, null, null, null},
+				{null, null, null, null, null, null, null, null, null},
+				{null, null, null, null, null, null, null, null, null},
+				{null, null, null, null, null, null, null, null, null},
+				{null, null, null, null, null, null, null, null, null},
+				{null, null, null, null, null, null, null, null, null},
+				{null, null, null, null, null, null, null, null, null},
+			},
+			new String[] {
+				"Spieler", "E1", "X1", "Y1", "E2", "X2", "Y2", "Mühle", "Gelöschter Stein"
+			}
+		));
+		scrollPane.setViewportView(table);
 	}
-
+	
 }
